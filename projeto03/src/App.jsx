514e76188c0f83f5da8b924ecb8471742fd704ff
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 17:58:10 -0300

    Add files via upload

---
+++ projeto03/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'; 
+
+import RelogioDigital from './Components/Projeto03/Projeto03'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <RelogioDigital /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App;
